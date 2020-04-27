fef979a0b6eb1ca7cc4f17d367e00dc0db13e6ca
Diego Alvarado <diego.alvarado@wolvenhall.com>
2020-04-27 18:18:13 +0000

    API create partner

--- api_jsonrpc/controllers/controllers.py
+++ api_jsonrpc/controllers/controllers.py
@@ -1,12 +1,57 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+from odoo.http import Controller, request
 
-
-# class ApiJsonrpc(http.Controller):
-#     @http.route('/api_jsonrpc/api_jsonrpc/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
+class ApiJsonrpc(http.Controller):
+    @http.route('/api/auth', type='json', auth='none', methods=["POST"])
+    def authenticate(self, db, login, password):        
+        request.session.authenticate(db, login, password)
+        return request.env['ir.http'].session_info()
+    
+    @http.route('/api/get_contacts', type='json', auth='user')
+    def get_contacts(self):
+        contact_rec = request.env['res.partner'].search([])
+        contacts = []
+        for rec in contact_rec:
+            vals = {
+                'id': rec.id,
+                'name': rec.name
+            }
+            contacts.append(vals)
+        data = {'status': 200, 'response': contacts, 'message': 'Success'}
+        return data
+    
+    @http.route('/api/create_partner', type='json', auth='user')
+    def create_contacts(self, **rec):
+        global response
+        if request.jsonrequest:
+            
+            if rec['name'] and rec['vat']:
+                vals = {
+                    'name': rec['name'],
+                    'vat': rec['vat'],
+                    'website': rec['website'],
+                    'street': rec['street'],
+                    'street2': rec['street2'],
+                    'zip': rec['zip'],
+                    'city': rec['city'],
+                    'state_id': rec['state_id'],
+                    'email': rec['email'],
+                    'phone': rec['phone'],
+                    'mobile': rec['mobile'],
+                    'is_company': rec['is_company']
+                }
+                try:
+                    new_contact = request.env['res.partner'].sudo().create(vals)
+                    response = {'success': True, 'message': 'Success', 'ID': new_contact.id}
+                except ValueError:
+                    print(ValueError)
+                    response = {'success': True, 'message': 'Error al insertar valores'}
+            else:
+                response = {'success': True, 'message': 'Faltan valores'}
+        args = response
+        return args           
+                        
 #     @http.route('/api_jsonrpc/api_jsonrpc/objects/', auth='public')
 #     def list(self, **kw):
 #         return http.request.render('api_jsonrpc.listing', {
