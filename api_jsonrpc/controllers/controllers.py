d78a50e8ad8275ce86c52e3638ef3cf3ff126ea0
Diego Alvarado <diego.alvarado@wolvenhall.com>
2020-04-29 04:32:07 +0000

    Creane new Approval API

--- api_jsonrpc/controllers/controllers.py
+++ api_jsonrpc/controllers/controllers.py
@@ -64,7 +64,7 @@ class ApiJsonrpc(http.Controller):
                     # 2. Search Tag
                     id_category = request.env['res.partner.category'].search([('name', '=', 'Asociada')], limit=1).id
 
-                    # 3. If category exist
+                    # 3. If tag exist
                     if id_category > 0:
                         request.cr.execute("""insert into res_partner_res_partner_category_rel(category_id,partner_id) 
                                            values(""" + str(id_category) + """,""" + str(new_contact.id) + """)""")
@@ -73,11 +73,10 @@ class ApiJsonrpc(http.Controller):
                         category = {'name': 'Asociada'}
                         request.env['res.partner.category'].sudo().create(category)
 
-                    response = {'success': True, 'message': 'Success', 'ID': new_contact.id}
+                    response = {'success': True, 'message': 'Company successfully created', 'ID': new_contact.id}
 
-                except ValueError:
-                    print(ValueError)
-                    response = {'success': True, 'message': ValueError}
+                except Exception as e:
+                    response = {'success': True, 'message': e}
 
             else:
                 response = {'success': True, 'message': 'Incomplete values'}
@@ -92,6 +91,7 @@ class ApiJsonrpc(http.Controller):
 
         if request.jsonrequest:
             if rec['name']:
+                
                 if rec['company']:
                     id_company = request.env['res.partner'].search([('name', 'ilike', rec['company'])], limit=1).id
                     if id_company is False:
@@ -126,7 +126,7 @@ class ApiJsonrpc(http.Controller):
 
                 try:
 
-                    # 1. Insert new Partner
+                    # 1. Insert new Contact
                     new_contact = request.env['res.partner'].sudo().create(vals)
 
                     # 2. Search Tag
@@ -142,16 +142,82 @@ class ApiJsonrpc(http.Controller):
                         category = {'name': 'Asociada'}
                         request.env['res.partner.category'].sudo().create(category)
 
-                    response = {'success': True, 'message': 'Success', 'ID': new_contact.id}
+                    response = {'success': True, 'message': 'Contact successfully created', 'ID': new_contact.id}
 
-                except ValueError:
-                    print(ValueError)
-                    response = {'success': True, 'message': ValueError}
+                except Exception as e:
+                    response = {'success': True, 'message': e}
 
             else:
                 response = {'success': True, 'message': 'Incomplete values'}
 
             args = response
             return args
+        
+    @http.route('/api/create_approval', type='json', auth='user')
+    def create_approval(self, **rec):
+
+        global response, id_company, id_category
+
+        if request.jsonrequest:
+            
+            if rec['name']:
+                
+                id_category = request.env['approval.category'].search([('name', '=', 'Solicitud de Donativo')], limit=1).id
+                
+                if id_category > 0: 
+                    
+                    if rec['company']:
+                        id_company = request.env['res.partner'].search([('name', 'ilike', rec['company'])], limit=1).id
+                        if id_company is False:
+                            id_company = ''
+                    
+                    user_id = request.env['res.users'].sudo().search([('login', '=', rec['approval'])], limit=1).id
+                    
+                    vals = {
+                        'name': rec['name'],
+                        'category_id': id_category,
+                        'date': rec['date'],
+                        'quantity': '1',
+                        'partner_id': id_company,
+                        'reference': rec['reference'],
+                        'amount': rec['amount'],
+                        'reason': rec['reason'],
+                        'request_status': 'new',
+                        'request_owner_id': user_id
+                    }
+                    
+                    try:
+                        
+                        # 1. Create new Request
+                        
+                        new_request = request.env['approval.request'].sudo().create(vals)                    
+                                        
+                        # 2. Create new Approval
+                         
+                        request.env['approval.approver'].sudo().create({
+                            'user_id': user_id,
+                            'request_id': new_request.id,
+                            'status': 'new'})
+                    
+                        # Send Email to Request Aproval
+                        # mail_template = request.env['mail.template'].search([('name', 'like', 'Approval_Template')])
+                        # mail_template.write({'email_to': rec['approval'],})
+                        # mail_template.send_mail(user_id, force_send = True)
+
+                        response = {'success': True, 'message': 'Success', 'ID': new_request.id}    
+                    
+                    except Exception as e:
+                        print(e)
+                        response = {'success': True, 'message': e}
+                
+                else:
+                    response = {'success': True, 'message': 'We cant find the category, please create it and try again'}
+
+            else:
+                response = {'success': True, 'message': 'Incomplete values'}
+
+            args = response
+            return args   
+        
 
                    
